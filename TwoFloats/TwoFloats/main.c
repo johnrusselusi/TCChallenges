c6307facd4907032984d238d2c89ea6809d11fa7
John Russel Usi <j.usi@klab.com>
2014-09-10 09:19:55 +0800

    Added TwoFloats

---
+++ TwoFloats/TwoFloats/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  TwoFloats
+//
+//  Created by John Russel Usi on 9/10/14.
+//  Copyright (c) 2014 John Russel Usi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    float x = 3.14;
+    float y = 42.0;
+    float sum = x + y;
+    printf("%f\n", sum);
+    return 0;
+}
+
