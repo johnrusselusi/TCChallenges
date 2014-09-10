cb65a570994873146bfb932d3e6f5a578028d469
John Russel Usi <j.usi@klab.com>
2014-09-10 09:54:29 +0800

    Added Integers

---
+++ Integers/Integers/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  Integers
+//
+//  Created by John Russel Usi on 9/10/14.
+//  Copyright (c) 2014 John Russel Usi. All rights reserved.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, const char * argv[])
+{
+
+    double y = 12345.6789;
+    printf("y is %.2f\n", y);
+    printf("y is %.2e\n", y);
+    printf("%.3f\n", sin(1));
+    return 0;
+}
+
