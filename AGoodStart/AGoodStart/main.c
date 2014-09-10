ace4512f74afb0c1955584a9bf39ae6ea5c83723
John Russel Usi <j.usi@klab.com>
2014-09-10 09:09:11 +0800

    Added AGoodStart Program

---
+++ AGoodStart/AGoodStart/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by John Russel Usi on 9/10/14.
+//  Copyright (c) 2014 John Russel Usi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    //  The beginning of the novel
+    printf("It was the best of times.\n");
+    printf("It was the worst of times\n");
+    /*  Is that actually any good?
+        Maybe it needs a rewrite.   */
+    return 0;
+}
+
